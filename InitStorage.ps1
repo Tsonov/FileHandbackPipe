70b043cb5e6acb9c845c49d3d3749e2681ed8dd8
Lachezar Tsonov <tzonov_@abv.bg>
2014-11-23 17:57:43 +0000

    Updated InitStorage to create a queue as well

--- InitStorage.ps1
+++ InitStorage.ps1
@@ -4,9 +4,15 @@ $accountName = "filehb"
 $location = "West Europe"
 $handbackContainer = "handback"
 $processedContainer = "processedblobs"
+$handbackQueue = "handbackqueue"
 
 New-AzureStorageAccount -StorageAccountName $accountName -Location $location
-$storageKey = Get-AzureStorageKey -StorageAccountName $accountName | %{$_.Primary} # Get the primary key from the Primary property only
+$storageKey = Get-AzureStorageKey -StorageAccountName $accountName | %{$_.Primary} # Get the primary key only
 $storContext = New-AzureStorageContext -StorageAccountName $accountName -StorageAccountKey $storageKey -Protocol Https
+
+# Create the containers for the blobs
 New-AzureStorageContainer -Name $handbackContainer -Permission Off -Context $storContext
 New-AzureStorageContainer -Name $processedContainer -Permission Off -Context $storContext
+
+# Create the queue used for messaging to the worker roles
+New-AzureStorageQueue -Name $handbackqueue -Context $storContext
